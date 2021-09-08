14d751d2ad9bbc5bac5f4a0d5f328c2d540b7f48
jose walace oliveira baieta <75332020+Horaquytsume@users.noreply.github.com>
2021-09-08 16:34:39 -0300

    Update usuario.sql

--- usuario.sql
+++ usuario.sql
@@ -2,12 +2,25 @@ show databases;
 create database usuarios;
 use dbinfox;
 create database dbinfox;
-
+describe usuarios;
 create table usuario(
 id int primary key auto_increment,
 usuario varchar (50) not null,
-login varchar (10) not null,
+login varchar (10) not null unique,
 senha varchar (10)
 );
+drop tables usuario;
 show tables;
 describe usuario;
+insert into usuarios (usuario,login,senha)
+values ('walace','admin','1234');
+insert into usuarios (usuario,login,senha)
+values ('dante','admin','1234');
+
+-- armezenando um campo com criptografia
+insert into usuario(usuarios,login,senha)
+values ('walace','walace@usuario',md5('1234'));
+alter table usuarios modify senha varchar (250);  
+alter table usuarios modify login varchar(50);
+
+select*from usuarios;
